04d2bff40a355fdd012469b5edc0eb7fb26ed686
marty-p <marty-p@outlook.com>
2016-11-27 04:31:55 +0100

    +LL09_Es5.c fix

--- Uni/PR1/LL09_Es5.c
+++ Uni/PR1/LL09_Es5.c
@@ -3,29 +3,22 @@
 /*5. Scrivere una funzione che esegua l'acquisizione di un numero e il controllo che il numero si trovi all'interno di un
 determinato intervallo (min, max), mediante l'acquisizione ripetuta del numero. La funzione deve restituire sempre un valore
 compreso tra min e max.*/
-#include <time.h>
-int randrange(int min, int max);
+#define MIN 1
+#define MAX 100
+int acquisizioneNumero(int min, int max);
 int main()
 {
-    int cdInputMin;
-    int cdInputMax;
-    srand(time(NULL));
-    do
-    {
-        printf("Inserisci un numero minimo:\n");
-        scanf("%d", &cdInputMin);
-    }
-    while (cdInputMin <= 0);
-    do
-    {
-        printf("Inserisci un numero massimo maggiore di %d:\n", cdInputMin);
-        scanf("%d", &cdInputMax);
-    }
-    while (cdInputMax <= cdInputMin);
-    printf("%d\n", randrange(cdInputMin, cdInputMax));
+    printf("Numero: %d\n", acquisizioneNumero(MIN, MAX));
     return 0;
 }
-int randrange(int min, int max)
+int acquisizioneNumero(int min, int max)
 {
-    return min + rand() % (max-min+1);
+    int nInput;
+    do
+    {
+        printf("Inserisci un numero tra %d e %d:\n", min, max);
+        scanf("%d", &nInput);
+    } while (nInput < min || nInput > max);
+    return nInput;
 }
+
