74539e81aaef031a7e6f6a774466b1ed29ab3237
marty-p <marty-p@outlook.com>
2016-11-30 07:20:51 +0100

    +LL11_Es1.c

--- Uni/PR1/LL11_Es1.c
+++ Uni/PR1/LL11_Es1.c
@@ -1,38 +1,104 @@
 #include <stdio.h>
 #include <stdlib.h>
-/*1. Implementare una struttura vector2D composta da due componenti x e y. Implementare una
-funzione per la somma e il prodotto, componente per componente, di due vettori.
-Implementare successivamente una struttura e le relative operazioni che gestisce un
-vector3D*/
-#define N 5
+/*1. Implementare una struttura Vector2D composta da due componenti x e y. Implementare una funzione per la somma e il prodotto, componente per componente, di due vettori.
+Implementare successivamente una struttura e le relative operazioni che gestisce un Vector3D.*/
+typedef struct {
+    float x;
+    float y;
+} Vector2D;
 
-typedef struct vector2D_s {
-	int x;
-	int y;
-} vector2D;
+typedef struct {
+    float x;
+    float y;
+    float z;
+} Vector3D;
 
-typedef struct vector3D_s {
-	int x;
-	int y;
-	int z;
-} vector3D;
+Vector2D somma(Vector2D a1, Vector2D a2);
+void somma_ptr(Vector2D a1, Vector2D a2, Vector2D * res);
+Vector2D prodotto(Vector2D a1, Vector2D a2);
+void prodotto_ptr(Vector2D a1, Vector2D a2, Vector2D * res);
+void printa_v2d(Vector2D * a1);
 
-int sum_2d(vector2D a1[], vector2D a2[], int n);
+Vector3D somma3d(Vector3D a1, Vector3D a2);
+void somma3d_ptr(Vector3D a1, Vector3D a2, Vector3D * res);
+Vector3D prodotto3d(Vector3D a1, Vector3D a2);
+void prodotto3d_ptr(Vector3D a1, Vector3D a2, Vector3D * res);
+void printa_v3d(Vector3D * a1);
 
 int main()
 {
-	vector2D array1[N] = {{11, 22},{11, 22},{11, 22},{11, 22},{11, 22},};
-	vector2D array2[N] = {{33, 44},{55, 66},{77, 88},{99, 00},{111, 122},};
-	int i;
-	int somma = 0;
-	for (i=0; i<N; i++)
-	{
-		sum_2d(array1[i], array2[i]);
-	}
-	return 0;
+    Vector2D a1 = {11, 22};
+    Vector2D a2 = {33, 44};
+    Vector2D res_somma;
+    Vector2D res_prodotto;
+
+    Vector3D a1_3 = {11, 22, 33};
+    Vector3D a2_3 = {44, 55, 66};
+    Vector3D res_somma3;
+    Vector3D res_prodotto3;
+
+    somma_ptr(a1, a2, &res_somma);
+    printa_v2d(&res_somma);
+    prodotto_ptr(a1, a2, &res_prodotto);
+    printa_v2d(&res_prodotto);
+
+    somma3d_ptr(a1_3, a2_3, &res_somma3);
+    printa_v3d(&res_somma3);
+    prodotto3d_ptr(a1_3, a2_3, &res_prodotto3);
+    printa_v3d(&res_prodotto3);
+
+    return 0;
 }
 
-int sum_2d(vector2D * a1, vector2D * a2)
+Vector2D somma(Vector2D a1, Vector2D a2)
 {
-	return a1.x + a2.x
+    Vector2D res = {};
+    res.x = a1.x + a2.x;
+    res.y = a1.y + a2.y;
+    return res;
 }
+
+void somma_ptr(Vector2D a1, Vector2D a2, Vector2D * res)
+{
+    res->x = a1.x + a2.x;
+    res->y = a1.y + a2.y;
+}
+
+Vector2D prodotto(Vector2D a1, Vector2D a2)
+{
+    Vector2D res = {};
+    res.x = a1.x * a2.x;
+    res.y = a1.y * a2.y;
+    return res;
+}
+
+void prodotto_ptr(Vector2D a1, Vector2D a2, Vector2D * res)
+{
+    res->x = a1.x * a2.x;
+    res->y = a1.y * a2.y;
+}
+
+void printa_v2d(Vector2D * a1)
+{
+    printf("x: %.2f, y: %.2f\n", a1->x, a1->y);
+}
+
+void somma3d_ptr(Vector3D a1, Vector3D a2, Vector3D * res)
+{
+    res->x = a1.x + a2.x;
+    res->y = a1.y + a2.y;
+    res->z = a1.z + a2.z;
+}
+
+void prodotto3d_ptr(Vector3D a1, Vector3D a2, Vector3D * res)
+{
+    res->x = a1.x * a2.x;
+    res->y = a1.y * a2.y;
+    res->z = a1.z * a2.z;
+}
+
+void printa_v3d(Vector3D * a1)
+{
+    printf("x: %.2f, y: %.2f, z: %.2f\n", a1->x, a1->y, a1->z);
+}
+
