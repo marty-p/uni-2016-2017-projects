f72a2f5ee90dd072fec82d88702b2ffdf7eb8af9
marty-p <marty-p@outlook.com>
2017-01-29 19:03:45 +0100

    +djanni core_game

---
+++ Uni/PR1/exploding-djanni/core_game.c
@@ -0,0 +1,6 @@
+#include "core_game.h"
+
+_Bool core_game_save_game(Player pPlayers[], int players_count, CardDeck * pDeck, GameStatus * pStatus)
+{
+}
+
---
+++ Uni/PR1/exploding-djanni/core_game.h
@@ -0,0 +1,11 @@
+#ifndef __EDJ_CORE_GAME_H__
+#define __EDJ_CORE_GAME_H__
+
+/* include list */
+#include "core.h"
+
+/* prototype list */
+extern _Bool core_game_save_game(Player pPlayers[], int players_count, CardDeck * pDeck, GameStatus * pStatus);
+
+#endif
+
