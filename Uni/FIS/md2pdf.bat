66d834ce95a9560435b9a1c25b86b33c940dc5a6
marty-p <marty-p@outlook.com>
2017-03-09 04:14:43 +0100

    +fis es1 notes p1

---
+++ Uni/FIS/md2pdf.bat
@@ -0,0 +1,37 @@
+@echo off
+set main_dir=%~dp0
+set md_dir="%1"
+set md_list=
+set pandoc_dir=
+
+SETLOCAL EnableDelayedExpansion
+
+cd %md_dir%
+echo deleting the old instances...
+del /F /Q notes.pdf
+
+for /R "." %%f in (*.md) do (
+	SET md_list=!md_list! %%f
+	echo added %%f in list...
+)
+
+echo processing %md_list% into a notes...
+
+%pandoc_dir%pandoc.exe %md_list% ^
+	-t latex --latex-engine=xelatex ^
+	--toc --toc-depth=6 ^
+	-V lang=it ^
+	-V geometry=a4paper --top-level-division=section ^
+	--dpi=300 ^
+	--variable fontsize=12pt ^
+	-o "notes.pdf"
+
+	REM --variable documentclass=report ^
+	REM -t latex --latex-engine=pdflatex ^
+	REM --wrap=none ^
+	REM --standalone --normalize --smart --self-contained ^
+	REM -V fontsize=11pt ^
+
+start notes.pdf
+cd %main_dir%
+REM pause
