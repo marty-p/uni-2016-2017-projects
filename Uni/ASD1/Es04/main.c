0b744c05d005df694d4b7b0d97dfc561edb842e2
marty-p <marty-p@outlook.com>
2017-03-29 18:53:31 +0200

    +asd1 es04 fix

--- Uni/ASD1/Es04/main.c
+++ Uni/ASD1/Es04/main.c
@@ -12,54 +12,54 @@ void stampa_insuccesso(int * insucc, int n)
 
 int * insuccesso(const char * pat)
 {
-    int i,j;
-    int n = strlen(pat);
+	int i,j;
+	int n = strlen(pat);
 	int * insucc = calloc(sizeof(int), n);
 	if (insucc==NULL)
 		exit(EXIT_FAILURE);
-    insucc[0] = -1;
-    // analisi dei prefissi
-    for (j = 1; j <= n-1; j++)
-    {
-        i = insucc[j-1];
-        while (pat[j] != pat[i+1] && i>=0)
-            i = insucc[i];
-        if (pat[j] == pat[i+1])
-            insucc[j] = i+1;
-        else
-            insucc[j] = -1;
-    }
-    return insucc;
+	insucc[0] = -1;
+	// analisi dei prefissi
+	for (j = 1; j <= n-1; j++)
+	{
+		i = insucc[j-1];
+		while (pat[j] != pat[i+1] && i>=0)
+			i = insucc[i];
+		if (pat[j] == pat[i+1])
+			insucc[j] = i+1;
+		else
+			insucc[j] = -1;
+	}
+	return insucc;
 }
 
 int KMP_match(const char * stringa, const char * pat)
 {
-    int * insucc = insuccesso(pat);// array insuccesso
-    int i = 0;
-    int j = 0;
-    int lenS = strlen(stringa);
-    int lenP = strlen(pat);
+	int * insucc = insuccesso(pat);// array insuccesso
+	int i = 0;
+	int j = 0;
+	int lenS = strlen(stringa);
+	int lenP = strlen(pat);
 	stampa_insuccesso(insucc, lenP);
-    while (i < lenS && j < lenP)
-    {
-        if (stringa[i] == pat[j])
-        {
-            i = i+1;
-            j = j+1;
-        }
-        else if (j == 0)
-            i = i+1;
-        else
-            j = insucc[j-1]+1;
-    }
+	while (i < lenS && j < lenP)
+	{
+		if (stringa[i] == pat[j])
+		{
+			i = i+1;
+			j = j+1;
+		}
+		else if (j == 0)
+			i = i+1;
+		else
+			j = insucc[j-1]+1;
+	}
 	free(insucc);
 
-    if (j == lenP)
-        return i-lenP;
-    return -1;
+	if (j == lenP)
+		return i-lenP;
+	return -1;
 }
 
-int KMP_cyclic_match_ok(const char * stringa, const char * pat)
+int KMP_cyclic_match_alternative(const char * stringa, const char * pat)
 {
 	int i=0, j=0, c=0;
 	int lenS=strlen(stringa), lenP=strlen(pat);
@@ -86,6 +86,8 @@ int KMP_cyclic_match_ok(const char * stringa, const char * pat)
 		if (pat[i]==stringa[j])
 			j++;
 	}
+	free(insucc);
+
 	if (j>=lenS)
 		return i;
 
@@ -100,28 +102,27 @@ int KMP_cyclic_match(const char * stringa, const char * pat)
 	if (lenS!=lenP)
 		return -1;
 	insucc = insuccesso(pat);
-	stampa_insuccesso(insucc, lenP);
+	// stampa_insuccesso(insucc, lenP);
 
-    while (j < lenP)// && !(i==lenP-1 && j==0))
-    {
-		printf("i=%d, j=%d\n", i, j);
-		printf("stringa[i]=%c, pat[j]=%c\n", stringa[i], pat[j]);
-        if (stringa[i] == pat[j])
-        {
-            i = i+1;
-            j = (j+1);//%lenS;
-        }
-        else if (j == 0)
-            i = i+1;
-        else
-            j = (insucc[j-1]+1);//%lenS;
-		i%=lenP;
-    }
+	while (i < lenS*2 && j < lenP)
+	{
+		// printf("i=%d, j=%d\n", i, j);
+		// printf("stringa[i]=%c, pat[j]=%c\n", stringa[i%lenP], pat[j]);
+		if (stringa[i%lenP] == pat[j])
+		{
+			i = i+1;
+			j = j+1;
+		}
+		else if (j == 0)
+			i = i+1;
+		else
+			j = insucc[j-1]+1;
+	}
 	free(insucc);
 
-	printf("XXi=%d, j=%d\n", i, j);
-    if (j == lenP)
-        return lenP-i;
+	// printf("XXi=%d, j=%d\n", i, j);
+	if (j == lenP)
+		return (lenP-i%lenP)%lenP;
 
 	return -1;
 }
@@ -130,14 +131,14 @@ int main(void)
 {
 	//printf("ACACACACACAGT has ACACAGT %d\n", KMP_match("ACACACACACAGT", "ACACAGT"));
 	//printf("ACACACACACAGT has ACACAJT %d\n", KMP_match("ACACACACACAGT", "ACACAJT"));
-	//printf("ACACACACACAGT==CAGTACACACACA %d\n", KMP_cyclic_match("ACACACACACAGT", "CAGTACACACACA"));
-	//printf("ACACACACACAGT==CAGTACACACACA %d\n", KMP_cyclic_match("ACACACACACAGT", "CAXTACACACACA"));
+	printf("ACACACACACAGT==CAGTACACACACA %d\n", KMP_cyclic_match("ACACACACACAGT", "CAGTACACACACA"));
+	printf("ACACACACACAGT==CAGTACACACACA %d\n", KMP_cyclic_match("ACACACACACAGT", "CAXTACACACACA"));
 	printf("asd==asd %d\n", KMP_cyclic_match("asd", "asd"));
 	printf("asd==das %d\n", KMP_cyclic_match("asd", "das"));
 	printf("asd==sda %d\n", KMP_cyclic_match("asd", "sda"));
 	printf("asd==daz %d\n", KMP_cyclic_match("asd", "daz"));
-	//printf("ciaone==neciao %d\n", KMP_cyclic_match("ciaone", "neciao"));
-	////printf("ciaone==leciao %d\n", KMP_cyclic_match("ciaone", "leciao"));
+	printf("ciaone==neciao %d\n", KMP_cyclic_match("ciaone", "neciao"));
+	printf("ciaone==leciao %d\n", KMP_cyclic_match("ciaone", "leciao"));
 	return 0;
 }
 
