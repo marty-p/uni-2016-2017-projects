06a16a1534bf420a5329f8e2e8c706e56fa4349e
marty-p <marty-p@outlook.com>
2017-06-07 11:09:00 +0200

    +es11 input

--- Uni/ASD1/Es11/main.c
+++ Uni/ASD1/Es11/main.c
@@ -1,54 +1,74 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#include <string.h>
 #include <time.h>
 #include <stdbool.h>
 #define MIN 0
 #define MAX 100
-#define N 100
-//#define N 20000
-#define N_LIST 6
 #define PCT_SORT_NEXT 80
 //#define ENABLE_PRINT_ARRAY
 //#define TEST_ALL_N
+#define ENABLE_QUICKINSERTION_INPUT
+
+#define TAB "\t"
+#define LN "\n"
+
+typedef int elem;
+typedef elem * array;
+
 #define SCHEMA_N 4
-//#define ENABLE_QUICKINSERTION_INPUT
-
 typedef enum {ORDINATO, QUASI_ORDINATO, INV_ORDINATO, CASUALE} inputType;
+inputType schema_list[SCHEMA_N] = {ORDINATO, QUASI_ORDINATO, INV_ORDINATO, CASUALE};
 
-void processAll(int n);
+#define N_LIST 6
+int nList[N_LIST] = {100, 1000, 10000, 100000, 200000, 500000};
+
+#define ALG_N 3
+typedef void (*algFunc)(array a, int n, inputType tipo_schema);
+typedef enum {INSERTSORT, QUICKSORT, QUICKINSERTSORT} algType;
+void insertSortWithTime(array a, int n, inputType tipo_schema);
+void quickSortWithTime(array a, int n, inputType tipo_schema);
+void quickinsertSortWithTime(array a, int n, inputType tipo_schema);
+algFunc algList[ALG_N] = {insertSortWithTime, quickSortWithTime, quickinsertSortWithTime};
+
+// TYPES
+const char * get_schema_name(inputType tipo_schema);
+const char * get_alg_name(algType tipo_alg);
+const char * get_n_name(int n);
+
+int get_schema_input();
+int get_alg_input();
+int get_n_input();
+
+// MAIN
+void processAll();
 int get_random_number(int min, int max);
-void print_array(int * a, int n);
-void swap_numbers(int * a, int n);
-int * genera_array(int dimensione, inputType tipo_schema);
-void insertionSortWithTime(int * lista, int dim, inputType tipo_schema);
-void insertionSort(int * lista, int dim);
-void quickSortWithTime(int * a, int n, inputType tipo_schema);
-void quickSort(int * lista, int u, int v);
-void quickinsertionSortWithTime(int * a, int n, inputType tipo_schema);
-void quickinsertionSort(int * lista, int u, int v, int M);
-int perno(int * lista, int primo, int ultimo);
-void swap(int * a, int * b);
-int findmin(int * A, int minpos, int start, int dim);
+void print_array(array a, int n);
+void swap_numbers(array a, int n);
+array genera_array(int dimensione, inputType tipo_schema);
+
+void insertionSort(array lista, int dim);
+void quickSort(array lista, int u, int v);
+void quickinsertionSort(array lista, int u, int v, int M);
+
+int perno(array lista, int primo, int ultimo);
+void swap(array a, array b);
+int findmin(array A, int minpos, int start, int dim);
 const char * getTipoSchemaName(inputType tipo_schema);
-_Bool check_sort(int * A, int n);
+_Bool check_sort(array A, int n);
 
 int main(int argc, char** argv)
 {
-#ifndef TEST_ALL_N
-	int n = N;
-#else
-	int i;
-	int nList[N_LIST] = {100, 1000, 10000, 100000, 200000, 500000};
-#endif
+	clock_t start;
 	srand(time(NULL));
-#ifndef TEST_ALL_N
-	processAll(n);
-#else
-	for (i=0; i<N_LIST; i++)
-		processAll(nList[i]);
-#endif
-	return (EXIT_SUCCESS);
+
+	start = clock();
+	// <chiamata all'algoritmo di ordinamento>
+	processAll();
+	// <fine chiamata all'algoritmo di ordinamento>
+	printf("Processed everything in %.4fs seconds\n", ((double)(clock()-start))/CLOCKS_PER_SEC);
+	return EXIT_SUCCESS;
 }
 
 int get_random_number(int min, int max)
@@ -56,7 +76,7 @@ int get_random_number(int min, int max)
 	return min + rand() % (max - min +1);
 }
 
-void print_array(int * a, int n)
+void print_array(array a, int n)
 {
 	int i;
 	for (i=0; i<n; i++)
@@ -64,56 +84,138 @@ void print_array(int * a, int n)
 	printf("\n");
 }
 
-void processAll(int n)
+const char * get_schema_name(inputType tipo_schema)
 {
-	int * A = NULL;
-	int * B = NULL;
-	int * C = NULL;
-	int i;
-	inputType schema_list[SCHEMA_N] = {ORDINATO, QUASI_ORDINATO, INV_ORDINATO, CASUALE};
+	static const char * schema_name[SCHEMA_N] = {"ORDINATO", "QUASI_ORDINATO", "INV_ORDINATO", "CASUALE"};
+	if (tipo_schema >= SCHEMA_N)
+		return "";
+	return schema_name[tipo_schema];
+}
 
-	printf("n=%d\n", n);
-	for (i=0; i<SCHEMA_N; i++)
+const char * get_alg_name(algType tipo_alg)
+{
+	static const char * alg_name[ALG_N] = {"INSERTSORT", "QUICKSORT", "QUICKINSERTSORT"};
+	if (tipo_alg >= ALG_N)
+		return "";
+	return alg_name[tipo_alg];
+}
+
+const char * get_n_name(int n)
+{
+	static const char * n_name[N_LIST] = {"100", "1000", "10000", "100000", "200000", "500000"};
+	if (n >= N_LIST)
+		return "";
+	return n_name[n];
+}
+
+int get_schema_input()
+{
+	int i;
+	do
 	{
-		A = genera_array(n, schema_list[i]);
-		B = genera_array(n, schema_list[i]);
-		C = genera_array(n, schema_list[i]);
-#ifdef ENABLE_PRINT_ARRAY
-		puts("Print A BEGIN:");
-		print_array(A, n);
-		puts("Print B BEGIN:");
-		print_array(B, n);
-		puts("Print C BEGIN:");
-		print_array(C, n);
-#endif
-		insertionSortWithTime(A, n, schema_list[i]);
-		quickSortWithTime(B, n, schema_list[i]);
-		quickinsertionSortWithTime(C, n, schema_list[i]);
-#ifdef ENABLE_PRINT_ARRAY
-		puts("Print A END:");
-		print_array(A, n);
-		puts("Print B END:");
-		print_array(B, n);
-		puts("Print C END:");
-		print_array(C, n);
-#endif
-		if (check_sort(A, n)==false)
-			puts("A wrong sort");
-		if (check_sort(B, n)==false)
-			puts("B wrong sort");
-		if (check_sort(C, n)==false)
-			puts("C wrong sort");
-		free(A);
-		free(B);
-		free(C);
+		printf("Schema Input:\n");
+		for (i=0; i<SCHEMA_N; i++)
+			printf("%d: %s\n", i, get_schema_name(i));
+		scanf("%d", &i);
+		getchar();
+	}
+	while (i<0 || i>=SCHEMA_N);
+	return i;
+}
+
+int get_alg_input()
+{
+	int i;
+	do
+	{
+		printf("Alg Input:\n");
+		for (i=0; i<ALG_N; i++)
+			printf("%d: %s\n", i, get_alg_name(i));
+		scanf("%d", &i);
+		getchar();
+	}
+	while (i<0 || i>=ALG_N);
+	return i;
+}
+
+int get_n_input()
+{
+	int i;
+	do
+	{
+		printf("N Input:\n");
+		for (i=0; i<N_LIST; i++)
+			printf("%d: %s\n", i, get_n_name(i));
+		scanf("%d", &i);
+		getchar();
+	}
+	while (i<0 || i>=N_LIST);
+	return i;
+}
+
+void processAll()
+{
+	array base = NULL;
+	array algs[ALG_N] = {0};
+	int i, j, k, n;
+
+	#ifdef TEST_ALL_N
+	for (k=0; k<N_LIST; k++)
+	#else
+	k = get_n_input();
+	#endif
+	{
+		#ifdef TEST_ALL_N
+		for (i=0; i<SCHEMA_N; i++)
+		#else
+		i = get_schema_input();
+		#endif
+		{
+			#ifdef TEST_ALL_N
+			puts("------------------------------");
+			#endif
+			n = nList[k];
+			base = genera_array(n, schema_list[i]);
+
+			#ifdef TEST_ALL_N
+			for (j=0; j<ALG_N; j++)
+			#else
+			j = get_alg_input();
+			#endif
+			{
+				algs[j] = calloc(n, sizeof(elem));
+				memcpy(algs[j], base, n*sizeof(elem));
+
+				#ifdef ENABLE_PRINT_ARRAY
+				printf("%s BEGIN:\n", get_alg_name(j));
+				print_array(algs[j], n);
+				#endif
+
+				algList[j](algs[j], n, schema_list[i]);
+
+				#ifdef ENABLE_PRINT_ARRAY
+				printf("%s END:\n", get_alg_name(j));
+				print_array(algs[j], n);
+				#endif
+
+				if (check_sort(algs[j], n)==false)
+					printf("%s wrong sort\n", get_alg_name(j));
+
+				free(algs[j]);
+				algs[j] = NULL;
+			}
+		}
+		#ifdef TEST_ALL_N
+		puts("------------------------------------------------------------");
+		#endif
 	}
 }
 
-int * genera_array(int dimensione, inputType tipo_schema)
+array genera_array(int dimensione, inputType tipo_schema)
 {
 	int i;
 	int prev_n = 0;
-	int * tmp_a = calloc(dimensione, sizeof(int));
+	array tmp_a = calloc(dimensione, sizeof(int));
 	if (tmp_a==NULL)
 	{
 		printf("memoria insufficiente per %d", dimensione);
@@ -145,7 +247,7 @@ int * genera_array(int dimensione, inputType tipo_schema)
 	return tmp_a;
 }
 
-void insertionSortWithTime(int * a, int n, inputType tipo_schema)
+void insertSortWithTime(array a, int n, inputType tipo_schema)
 {
 	clock_t start, end;
 	double tempo;
@@ -158,7 +260,7 @@ void insertionSortWithTime(int * a, int n, inputType tipo_schema)
 	printf("INSERTION_SORT TIPO: %s, N: %d, SECS: %f\n", getTipoSchemaName(tipo_schema), n, tempo);
 }
 
-void insertionSort(int * lista, int dim)
+void insertionSort(array lista, int dim)
 {
 	int i,j;
 	int prossimo;
@@ -176,7 +278,7 @@ void insertionSort(int * lista, int dim)
 	}
 }
 
-void quickSortWithTime(int * a, int n, inputType tipo_schema)
+void quickSortWithTime(array a, int n, inputType tipo_schema)
 {
 	clock_t start, end;
 	double tempo;
@@ -189,7 +291,7 @@ void quickSortWithTime(int * a, int n, inputType tipo_schema)
 	printf("QUICK_SORT TIPO: %s, N: %d, SECS: %f\n", getTipoSchemaName(tipo_schema), n, tempo);
 }
 
-void quickSort(int * lista, int u, int v)
+void quickSort(array lista, int u, int v)
 {
 	int q;
 	if (u == v) return;
@@ -198,7 +300,7 @@ void quickSort(int * lista, int u, int v)
 	if (q < v) quickSort(lista, q+1, v);
 }
 
-int perno(int * lista, int primo, int ultimo)
+int perno(array lista, int primo, int ultimo)
 {
 	int i,j;
 	int pivot;
@@ -207,8 +309,8 @@ int perno(int * lista, int primo, int ultimo)
 	pivot = lista[primo];
 	while (i < j)
 	{
-		do {i = i+1;} while (lista[i] <= pivot);
-		do {j = j-1;} while (lista[j] > pivot);
+		do {i = i+1;} while (lista[i] <= pivot && i <= ultimo);
+		do {j = j-1;} while (lista[j] > pivot && j >= primo);
 		if (i < j)
 			swap(&lista[i], &lista[j]);
 	}
@@ -216,7 +318,7 @@ int perno(int * lista, int primo, int ultimo)
 	return j;
 }
 
-void swap(int * a, int * b)
+void swap(array a, array b)
 {
 	int c;
 	if (a==NULL || b==NULL)
@@ -226,14 +328,17 @@ void swap(int * a, int * b)
 	*b = c;
 }
 
-void quickinsertionSortWithTime(int * a, int n, inputType tipo_schema)
+void quickinsertSortWithTime(array a, int n, inputType tipo_schema)
 {
 	clock_t start, end;
 	double tempo;
 	int M = 66;
 #ifdef ENABLE_QUICKINSERTION_INPUT
-	printf("numero fra %d e %d:\n", MIN, MAX);
-	scanf("%d", &M);
+	do
+	{
+		printf("numero fra %d e %d:\n", MIN, MAX);
+		scanf("%d", &M);
+	} while (M < MIN || M > MAX);
 #endif
 	start = clock();
 	// <chiamata all’algoritmo di ordinamento>
@@ -244,7 +349,7 @@ void quickinsertionSortWithTime(int * a, int n, inputType tipo_schema)
 	printf("QUICKINSERTION_SORT TIPO: %s, N: %d, SECS: %f\n", getTipoSchemaName(tipo_schema), n, tempo);
 }
 
-void quickinsertionSort(int * lista, int u, int v, int M)
+void quickinsertionSort(array lista, int u, int v, int M)
 {
 	int q;
 	if (u == v) return;
@@ -258,7 +363,7 @@ void quickinsertionSort(int * lista, int u, int v, int M)
 	}
 }
 
-int findmin(int * A, int minpos, int start, int dim)
+int findmin(array A, int minpos, int start, int dim)
 {
 	if (start == dim)
 		return minpos;
@@ -275,7 +380,7 @@ const char * getTipoSchemaName(inputType tipo_schema)
 	return schema_name[tipo_schema];
 }
 
-_Bool check_sort(int * A, int n)
+_Bool check_sort(array A, int n)
 {
 	int i;
 	_Bool isSorted = true;
