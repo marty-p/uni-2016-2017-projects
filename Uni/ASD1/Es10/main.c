f5e87c7f3c809beea5fb95ed20dade4a76c801dd
marty-p <marty-p@outlook.com>
2017-06-07 15:32:23 +0200

    +asd es10 ENABLE_INPUT_MODE

--- Uni/ASD1/Es10/main.c
+++ Uni/ASD1/Es10/main.c
@@ -2,10 +2,13 @@
 #include <stdlib.h>
 #include <string.h>
 
+#define ENABLE_INPUT_MODE
+
 #define MAX_FLOORS 10
+#define PLATE_LEN 7
 typedef struct car
 {
-	const char * plate_number;
+	char * plate_number;
 	struct car * next;
 } Car;
 typedef Car* lpCar;
@@ -19,18 +22,32 @@ void print_cars(const Car * node);
 void print_car(const char * plate_number);
 
 int hash_code(const char* plate_number);
-void insert_car(const char * plate_number);
+void insert_car(char * plate_number);
 lpCar search_car(const char * plate_number);
 void delete_car(const char * plate_number);
 lpCar delete_car_node(Car * node, const char * plate_number);
 
-Car * insert_car_testa(Car * node, const char* plate_number);
-Car * insert_car_coda(Car * node, const char* plate_number);
+Car * insert_car_testa(Car * node, char* plate_number);
+Car * insert_car_coda(Car * node, char* plate_number);
 
 void default_set();
 
+#define IM_NUM 5
+typedef enum {IM_INSERT, IM_SEARCH, IM_DELETE, IM_PRINT, IM_EXIT} ImType;
+const char * get_input_name(ImType i);
+void input_mode();
+void input_insert(void);
+void input_search(void);
+void input_delete(void);
+void input_print(void);
+typedef void (*ImFnc)(void);
+ImFnc inputFunc[IM_NUM] = {input_insert, input_search, input_delete, input_print, NULL};
+
 int main()
 {
+#ifdef ENABLE_INPUT_MODE
+	input_mode();
+#else
 	// test insert
 	default_set();
 	print_car_list();
@@ -49,7 +66,67 @@ int main()
 	delete_car("CC333CC");
 	printf("\n\n\n\n");
 	print_car_list();
+#endif
 	return 0;
+}
+
+const char * get_input_name(ImType i)
+{
+	static const char * names[IM_NUM] = {"Inserisci", "Ricerca", "Cancella", "Stampa", "ESCI"};
+	if (i>=IM_NUM)
+		return "";
+	return names[i];
+}
+
+void input_mode()
+{
+	ImType i;
+	do
+	{
+		printf("Che vuoi fare? (sbaglia per uscire)\n");
+		for (i=0; i<IM_NUM; i++)
+			printf("%u: %s\n", i, get_input_name(i));
+		scanf("%u", &i);
+		while (getchar()!='\n')
+			continue;
+		if (i>=IM_EXIT)
+			break;
+		inputFunc[i]();
+	} while (i<IM_NUM);
+}
+
+void input_insert()
+{
+	char * plate_number = calloc(sizeof(char), PLATE_LEN+1);
+	printf("Inserisci la targa da inserire: (len: %d)\n", PLATE_LEN);
+	scanf("%7s", plate_number);
+	getchar();
+	insert_car(plate_number);
+}
+
+void input_search()
+{
+	char buf[PLATE_LEN+1] = {};
+	printf("Inserisci la targa da ricercare: (len: %d)\n", PLATE_LEN);
+	scanf("%7s", buf);
+	getchar();
+	lpCar nodo = search_car(buf);
+	printf("nodo %s", (nodo) ? "trovato" : "non trovato");
+}
+
+void input_delete()
+{
+	char buf[PLATE_LEN+1] = {};
+	printf("Inserisci la targa da cancellare: (len: %d)\n", PLATE_LEN);
+	scanf("%7s", buf);
+	getchar();
+	delete_car(buf);
+}
+
+void input_print()
+{
+	printf("\n\n\n\n");
+	print_car_list();
 }
 
 //#define DEFAULT_N 4
@@ -57,7 +134,7 @@ int main()
 void default_set()
 {
 	//const char * lista_targhe[DEFAULT_N] = {"PI555TA", "CA220TO", "EG000AL", "GO000LE"};
-	const char * lista_targhe[DEFAULT_N] = {
+	char * lista_targhe[DEFAULT_N] = {
 		"AA111AA", "BB222BB", "CC333CC", "DD444DD", "EE555EE",
 		"FF666FF", "GG777GG", "HH888HH", "II999II", "JJ000JJ",
 		"FF566FF", "GG677GG", "HH988HH", "II299II", "JJ100JJ", //ripetuti sin da qua
@@ -116,21 +193,21 @@ int hash_code(const char * plate_number)
 }
 
 //Una funzione che permette di inserire una nuova auto (in O(1) );
-void insert_car(const char * plate_number)
+void insert_car(char * plate_number)
 {
 	int h = hash_code(plate_number);
 	carpark[h] = insert_car_coda(carpark[h], plate_number);
 }
 
-Car * insert_car_testa(Car * node, const char * plate_number)
+Car * insert_car_testa(Car * node, char * plate_number)
 {
 	Car * testa = calloc(sizeof(Car), 1);
-	testa->plate_number = plate_number;
+	testa->plate_number= plate_number;
 	testa->next = node;
 	return testa;
 }
 
-Car * insert_car_coda(Car * node, const char * plate_number)
+Car * insert_car_coda(Car * node, char * plate_number)
 {
 	Car * tmp = node;
 	while (tmp!=NULL && tmp->next!=NULL)
@@ -146,12 +223,12 @@ Car * insert_car_coda(Car * node, const char * plate_number)
 //nodo (Car*) che la rappresenta;
 lpCar search_car(const char * plate_number)
 {
-	int i;
+//	int i;
 	int h = hash_code(plate_number);
 	lpCar node = carpark[h];
 	while (node!=NULL)
 	{
-		if (node->plate_number==plate_number)
+		if (strcmp(node->plate_number, plate_number)==0)
 			return node;
 		node = node->next;
 	}
@@ -168,23 +245,25 @@ void delete_car(const char * plate_number)
 lpCar delete_car_node(Car * node, const char * plate_number)
 {
 	lpCar prev_tmp = NULL;
-	int h = hash_code(plate_number);
+//	int h = hash_code(plate_number);
 	lpCar tmp = node;
 	while (tmp!=NULL)
 	{
-		if (tmp->plate_number==plate_number)
+		if (strcmp(tmp->plate_number, plate_number)==0)
 		{
 			// testa
 			if (prev_tmp==NULL)
 			{
 				prev_tmp = tmp;
 				tmp = tmp->next;
+				free(prev_tmp->plate_number);
 				free(prev_tmp);
 				return tmp;
 			}
 			else
 			{
 				prev_tmp->next = (tmp!=NULL) ? tmp->next : NULL;
+				free(tmp->plate_number);
 				free(tmp);
 				break;
 			}
